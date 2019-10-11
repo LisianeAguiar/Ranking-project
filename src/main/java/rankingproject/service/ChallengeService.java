6ac899d739162761593558fd1e989de7cf61a052
lisianeaguiar <lisianecampos@MacBook-Pro-de-Lisiane.local>
2019-10-11 17:33:41 -0300

    challenge service: bugs fixed

--- src/main/java/rankingproject/service/ChallengeService.java
+++ src/main/java/rankingproject/service/ChallengeService.java
@@ -1,5 +1,8 @@
 package rankingproject.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import rankingproject.domain.*;
 import rankingproject.repository.ChallengeRepository;
 import rankingproject.repository.PlayerRepository;
@@ -7,36 +10,56 @@ import rankingproject.repository.PlayerRepository;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ChallengeService {
 
-    private ChallengeRepository challenges = new ChallengeRepository();
-    private PlayerRepository players = new PlayerRepository();
-    private Ranking ranking = new Ranking();
+    private ChallengeRepository challenges;
+    private PlayerRepository players;
+    private Ranking ranking;
+    private GameService gameService;
 
-    public void createChallenge(String challengerId, String challengedId) {
+    @Autowired
+    public ChallengeService(ChallengeRepository repository, PlayerRepository playerRepository, GameService gameService, Ranking ranking) {
+        this.challenges = repository;
+        this.players = playerRepository;
+        this.ranking = ranking;
+        this.gameService = gameService;
+    }
+
+    public Challenge getChallenge (String id) {
+         return challenges.findChallenge(id);
+    }
+
+    public List<Challenge> get() {
+        return challenges.getChallenges();
+    }
+
+    public Challenge createChallenge(String challengerId, String challengedId) {
 
         Player challenger = players.findPlayerById(challengerId);
         Player challenged = players.findPlayerById(challengedId);
+        Challenge challenge = null;
 
-        if (challenger.getPosition() - challenged.getPosition() < 3) {
+        if(challenger != null && challenged != null) {
+            if (challenger.getPosition() - challenged.getPosition() <= 3) {
 
-            String id = Double.toString(Math.random());
-            Challenge challenge = new Challenge(id, Status.WAITING, challengerId, challengedId);
-            challenges.save(challenge);
+                String id = Double.toString(Math.random());
+                challenge = new Challenge(id, Status.WAITING, challengerId, challengedId);
+                challenges.save(challenge);
+            }
         }
 
+        return challenge;
     }
 
     public void accept(String id, String challengedId) {
 
-        challenges.getChallenges();
         Challenge challenge = challenges.findChallenge(id);
 
         if(challenge.getChallenged().equals(challengedId)) {
 
             challenge.setStatus(Status.ACCEPTED);
-            GameService.createGame(challenge.getChallenger(), challengedId);
-
+            gameService.createGame(challenge.getChallenger(), challengedId);
         }
     }
 
--- src/main/java/rankingproject/service/GameService.java
+++ src/main/java/rankingproject/service/GameService.java
@@ -1,33 +1,91 @@
 package rankingproject.service;
 
+import org.springframework.stereotype.Service;
 import rankingproject.domain.Game;
 import rankingproject.domain.Player;
 import rankingproject.repository.GameRepository;
+import rankingproject.repository.PlayerRepository;
 
+import java.util.List;
+
+@Service
 public class GameService {
 
-    private static GameRepository games = new GameRepository();
+    private GameRepository games = new GameRepository();
+    private PlayerRepository players = new PlayerRepository();
+    private RankingService rankingService = new RankingService();
 
-    public static void createGame(String challenger, String challenged) {
+    public Game getGame(String id) {
+       return games.findGame(id);
+    }
 
-        String id = Double.toString(Math.random());
-        Game game = new Game(id, challenger, challenged, 0, 0);
+    public List<Game> get() {
+
+        return games.getGames();
+    }
+
+    public void createGame(String challenger, String challenged) {
+
+        Game game = new Game(challenger, challenged, 0, 0);
         games.saveGame(game);
 
     }
 
-    public void updateScore(String gameId, String playerId) {
+    public void updateChallengerScore(String gameId, String playerId) {
 
         Game game = games.findGame(gameId);
         int score = 0;
+        String loserId = "";
 
-        if (game.getChallenged().equals(playerId)) {
-            score = game.getChallengedScore();
-            game.setChallengedScore(score++);
+        if (game.getResult() == null) {
+
+            if (game.getChallenger().equals(playerId)) {
+                if (game.getChallengerScore() > 10) {
+                    game.setResult(playerId);
+                    //ganhou, atualiza o ranking
+                    loserId = game.getChallenged();
+                    rankingService.changePositions(playerId, loserId);
+
+                }
+                else {
+
+                    score = game.getChallengerScore();
+                    score++;
+                    game.setChallengerScore(score);
+
+
+                }
+            }
         }
-        score = game.getChallengerScore();
-        game.setChallengerScore(score++);
-
     }
+
+    public void updateChallengedScore(String gameId, String playerId) {
+
+        Game game = games.findGame(gameId);
+        int score = 0;
+        String loserId = "";
+
+        if (game.getResult() == null) {
+
+            if (game.getChallenged().equals(playerId)) {
+
+                if (game.getChallengedScore() > 10) {
+                    game.setResult(playerId);
+                    //ganhou, atualiza o ranking
+                    loserId = game.getChallenger();
+                    rankingService.changePositions(playerId, loserId);
+
+                }
+                else {
+                    score = game.getChallengedScore();
+                    score++;
+                    game.setChallengedScore(score);
+
+                }
+            }
+        }
+    }
+
+
 
 }
