07af68ed9c45f60434e2656c1fe59783a6360faa
lisianeaguiar <lisianecampos@MacBook-Pro-de-Lisiane.local>
2019-10-14 14:12:29 -0300

    class test for game created

--- src/test/java/rankingproject/GameServiceTest.java
+++ src/test/java/rankingproject/GameServiceTest.java
@@ -1,4 +1,24 @@
 package rankingproject;
 
+import org.mockito.InjectMocks;
+import rankingproject.domain.Ranking;
+import rankingproject.repository.ChallengeRepository;
+import rankingproject.repository.PlayerRepository;
+import rankingproject.service.ChallengeService;
+import rankingproject.service.GameService;
+
+import static org.mockito.Mockito.mock;
+
 public class GameServiceTest {
+
+    @InjectMocks
+    private ChallengeService service = mock(ChallengeService.class);
+
+    private ChallengeRepository repository = mock(ChallengeRepository.class);
+
+    private PlayerRepository playerRepository = mock(PlayerRepository.class);
+
+    private Ranking ranking = mock(Ranking.class);
+
+    private GameService gameService = mock(GameService.class);
 }
