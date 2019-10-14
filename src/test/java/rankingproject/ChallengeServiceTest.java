59665d9e0deb4756af67a95c762f53fb562b9a36
lisianeaguiar <lisianecampos@MacBook-Pro-de-Lisiane.local>
2019-10-14 15:59:27 -0300

    challengeServiceTest organized

--- src/test/java/rankingproject/ChallengeServiceTest.java
+++ src/test/java/rankingproject/ChallengeServiceTest.java
@@ -5,9 +5,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import rankingproject.domain.Challenge;
-import rankingproject.domain.Ranking;
-import rankingproject.domain.Status;
+import rankingproject.domain.*;
 import rankingproject.repository.ChallengeRepository;
 import rankingproject.repository.PlayerRepository;
 import rankingproject.service.ChallengeService;
@@ -25,13 +23,13 @@ public class ChallengeServiceTest {
 
     @InjectMocks
 
-    private ChallengeRepository repository = mock(ChallengeRepository.class);
+    private ChallengeRepository challengeRepository = mock(ChallengeRepository.class);
 
     private PlayerRepository playerRepository = mock(PlayerRepository.class);
 
     private GameService gameService = mock(GameService.class);
 
-    private ChallengeService service = new ChallengeService(repository, playerRepository, gameService);
+    private ChallengeService challengeService = new ChallengeService(challengeRepository, playerRepository, gameService);
 
     private Challenge challenge;
 
@@ -43,54 +41,38 @@ public class ChallengeServiceTest {
     @Test
     public void accept_shouldFail_playerIdDifferentFromChallangedId() {
 
-       // ChallengeService service = new ChallengeService(repository, playerRepository, gameService, ranking);
+        when(challengeRepository.findChallenge(anyString())).thenReturn(challenge);
 
-        //Challenge challenge = new Challenge("555", Status.WAITING, "123", "345");
-
-        when(repository.findChallenge(anyString())).thenReturn(challenge);
-
-        service.accept(challenge.getId(), "444");
+        challengeService.accept(challenge.getId(), "444");
         assertThat(challenge.getStatus(), is(Status.ACCEPTED));
     }
 
     @Test
     public void reject_shouldFail_playerIdDifferentFromChallangedId() {
 
-       // Challenge challenge = new Challenge("555", Status.WAITING, "123", "345");
+        when(challengeRepository.findChallenge(anyString())).thenReturn(challenge);
 
-        when(repository.findChallenge(anyString())).thenReturn(challenge);
-
-        service.accept(challenge.getId(), "444");
+        challengeService.accept(challenge.getId(), "444");
         assertThat(challenge.getStatus(), is(Status.REJECTED));
     }
 
     @Test
     public void accept_shouldPass_playerIdEqualsChallangedId() {
 
-      //  Challenge challenge = new Challenge("555", Status.WAITING, "123", "345");
+        when(challengeRepository.findChallenge("555")).thenReturn(challenge);
 
-        when(repository.findChallenge("555")).thenReturn(challenge);
-
-        service.accept(challenge.getId(), "345");
+        challengeService.accept(challenge.getId(), "345");
         assertThat(challenge.getStatus(), is(Status.ACCEPTED));
     }
 
     @Test
     public void reject_shouldPass_playerIdEqualsFromChallangedId() {
 
-      //  Challenge challenge = new Challenge("555", Status.WAITING, "123", "345");
+        when(challengeRepository.findChallenge(anyString())).thenReturn(challenge);
 
-        when(repository.findChallenge(anyString())).thenReturn(challenge);
-
-        service.accept(challenge.getId(), "345");
+        challengeService.accept(challenge.getId(), "345");
         assertThat(challenge.getStatus(), is(Status.ACCEPTED));
     }
 
-    //testar se a criacao de um desafio gera um game
-    @Test
-    public void shouldPass_challengeGeneratesGame() {
-
-       // Challenge challenge = new Challenge();
-    }
 
 }
