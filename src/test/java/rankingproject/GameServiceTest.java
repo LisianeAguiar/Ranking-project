2b511cbebdf855e9d073fa7c7e400222dc1700f9
lisianeaguiar <lisianecampos@MacBook-Pro-de-Lisiane.local>
2019-10-14 14:48:00 -0300

    tests for update score working

--- src/test/java/rankingproject/GameServiceTest.java
+++ src/test/java/rankingproject/GameServiceTest.java
@@ -1,24 +1,98 @@
 package rankingproject;
 
+import org.junit.Test;
 import org.mockito.InjectMocks;
+import rankingproject.domain.Game;
+import rankingproject.domain.Player;
 import rankingproject.domain.Ranking;
 import rankingproject.repository.ChallengeRepository;
+import rankingproject.repository.GameRepository;
 import rankingproject.repository.PlayerRepository;
 import rankingproject.service.ChallengeService;
 import rankingproject.service.GameService;
+import rankingproject.service.PlayerService;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GameServiceTest {
 
     @InjectMocks
-    private ChallengeService service = mock(ChallengeService.class);
 
-    private ChallengeRepository repository = mock(ChallengeRepository.class);
+    private GameRepository gameRepository = mock(GameRepository.class);
 
-    private PlayerRepository playerRepository = mock(PlayerRepository.class);
+    private PlayerService playerService = mock(PlayerService.class);
+    
+    private GameService gameService = new GameService(gameRepository,playerService );
 
-    private Ranking ranking = mock(Ranking.class);
+    @Test
+    public void shouldPass_challengerScoreUpdates() {
 
-    private GameService gameService = mock(GameService.class);
+        Player challenger = new Player("Lis", "666", 2);
+        Player challenged = new Player("Gui", "999", 1);
+
+        Game game = new Game("666","999", 0, 0);
+
+        when(gameRepository.findGame(anyString())).thenReturn(game);
+
+        int scoreBeforeUpdate = game.getChallengerScore();
+        gameService.updateChallengerScore(game.getId(), challenger.getId());
+
+        assertThat(game.getChallengerScore(), is(scoreBeforeUpdate+1));
+
+    }
+
+    @Test
+    public void shouldFail_challengerScoreDoesntUpdate() {
+
+        Player challenger = new Player("Lis", "666", 2);
+        Player challenged = new Player("Gui", "999", 1);
+
+        Game game = new Game("666","999", 0, 0);
+
+        when(gameRepository.findGame(anyString())).thenReturn(game);
+
+        int scoreBeforeUpdate = game.getChallengerScore();
+        gameService.updateChallengerScore(game.getId(), challenger.getId());
+
+        assertThat(game.getChallengerScore(), is(scoreBeforeUpdate));
+
+    }
+
+    @Test
+    public void shouldPass_challengedScoreUpdates() {
+
+        Player challenger = new Player("Lis", "666", 2);
+        Player challenged = new Player("Gui", "999", 1);
+
+        Game game = new Game("666","999", 0, 0);
+
+        when(gameRepository.findGame(anyString())).thenReturn(game);
+
+        int scoreBeforeUpdate = game.getChallengedScore();
+        gameService.updateChallengedScore(game.getId(), challenged.getId());
+
+        assertThat(game.getChallengedScore(), is(scoreBeforeUpdate+1));
+
+    }
+
+    @Test
+    public void shouldFail_challengedScoreDoesntUpdate() {
+
+        Player challenger = new Player("Lis", "666", 2);
+        Player challenged = new Player("Gui", "999", 1);
+
+        Game game = new Game("666","999", 0, 0);
+
+        when(gameRepository.findGame(anyString())).thenReturn(game);
+
+        int scoreBeforeUpdate = game.getChallengedScore();
+        gameService.updateChallengedScore(game.getId(), challenged.getId());
+
+        assertThat(game.getChallengedScore(), is(scoreBeforeUpdate));
+
+    }
 }
