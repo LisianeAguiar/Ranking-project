c79509aa334d3c7f4327a3da2a1c51dd59c41a09
lisianeaguiar <lisianecampos@MacBook-Pro-de-Lisiane.local>
2019-10-15 14:40:50 -0300

    method save created

--- src/main/java/rankingproject/repository/PlayerRepository.java
+++ src/main/java/rankingproject/repository/PlayerRepository.java
@@ -25,6 +25,9 @@ public class PlayerRepository {
     }
 
 
+    public void save(Player player) {
+        players.add(player);
+    }
 
     public List<Player> getPlayers() {
         return players;
