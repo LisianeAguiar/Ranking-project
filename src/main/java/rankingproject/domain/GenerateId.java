56c2801ed5bf4548f60c87efcdb810850ffdefb0
lisianeaguiar <lisianecampos@MacBook-Pro-de-Lisiane.local>
2019-10-15 16:52:12 -0300

    generateId class created

--- src/main/java/rankingproject/domain/GenerateId.java
+++ src/main/java/rankingproject/domain/GenerateId.java
@@ -1,4 +1,15 @@
 package rankingproject.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Component
 public class GenerateId {
+
+
+    public String generateId() {
+
+        return Double.toString(Math.random());
+    }
 }
